67a3bfaa2db18761b09ab9d02091091bf3af04dc
ebinsanty <47107330+ebinsanty@users.noreply.github.com>
2019-06-20 16:38:51 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express=require("express");
 var app=express();
+var methodOverride=require("method-override");
 var bodyParser=require("body-parser");
 var mongoose=require("mongoose");
 mongoose.connect("mongodb://localhost/blog",{useNewUrlParser:true});
@@ -7,6 +8,7 @@ mongoose.connect("mongodb://localhost/blog",{useNewUrlParser:true});
 app.use(bodyParser.urlencoded({extended:true}));//usually copy paste line these 3 lines
 app.use(express.static("public"));
 app.set("view engine","ejs");
+app.use(methodOverride("_method"));
 
 var blogSchema=new mongoose.Schema({
 	title:String,
@@ -18,11 +20,83 @@ var blogSchema=new mongoose.Schema({
 
 var Blog=mongoose.model("Blog",blogSchema);
 
-app.get("/blog",function(req,res){
-	res.render("index");
+app.get("/",function(req,res){
+	res.redirect("/blog");
 })
+
+
+app.get("/blog",function(req,res){
+	Blog.find({},function(err,blogs){
+		if(err)
+			console.log(err)
+		else
+			res.render("index",{blogs:blogs});
+
+	});
+	
+});
+
+app.post("/blog",function(req,res){
+	Blog.create(req.body.blog,function(err,newBlog){
+		if(err)
+			console.log(err);
+		else
+			res.redirect("/blog")
+
+	});
+
+});
+
+app.put("/blog/:id",function(req,res){
+	Blog.findByIdAndUpdate(req.params.id,req.body.blog,function(err,foundBlog){
+		if(err)
+			console.log(err);
+		else
+			res.redirect("/blog");
+
+	});
+	
+
+});
+
+app.delete("/blog/:id",function(req,res){
+	Blog.findByIdAndRemove(req.params.id,function(err){
+		if(err)
+			console.log(err);
+		else
+			res.redirect("/blog");
+
+	});
+});
+
+app.get("/blog/new",function(req,res){
+	res.render("new");
+
+});
+
+app.get("/blog/:id",function(req,res){
+	Blog.findById(req.params.id,function(err,foundBlog){
+		if(err)
+			console.log(err);
+		else
+			res.render("show",{fBlog:foundBlog});
+
+	});
+	
+});
+
+app.get("/blog/:id/edit",function(req,res){
+	Blog.findById(req.params.id,function(err,foundBlog){
+		if(err)
+			console.log(err)
+		else
+			res.render("edit",{blog:foundBlog});
+	})
+
+});
+
 
 
 app.listen(3000,function(){
 	console.log("Blog server have started");
-});
+}); 
